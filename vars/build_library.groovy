3f3502388f172f23f8236b13430fe032ea7a29c7
chengyh2golang <44278186+chengyh2golang@users.noreply.github.com>
2020-06-18 11:37:22 +0800

    Update build_library.groovy

--- vars/build_library.groovy
+++ vars/build_library.groovy
@@ -24,21 +24,46 @@
           descriptor.setVersion "org.wangqing:zipkin-service","1.0.$BUILD_NUMBER"
           descriptor.transform()
           rtMaven.tool = 'maven363'
-          //rtMaven.run pom: './pom.xml', goals: 'clean install',buildInfo: buildInfo
-          rtMaven.run pom: './pom.xml', goals: 'clean package',buildInfo: buildInfo
+          rtMaven.run pom: './pom.xml', goals: 'clean install',buildInfo: buildInfo
+          //rtMaven.run pom: './pom.xml', goals: 'clean package',buildInfo: buildInfo
       }
 
-      //stage('deploy to test') {
-      //    buildInfo.env.capture = true
-     //     rtMaven.deployer.deployArtifacts buildInfo
-     //     artiServer.publishBuildInfo buildInfo
-     // }
+      stage('deploy to test') {
+          buildInfo.env.capture = true
+          rtMaven.deployer.deployArtifacts buildInfo
+          artiServer.publishBuildInfo buildInfo
+      }
 
       stage('jira') {}
 
-      stage('test') {
-
-      }
+      stage('unit test') {
+        junit (testResults: '**/surefire-reports/**/*Test.xml')
+        //def build = manager.build
+        def testDatas = manager.build.getAction(hudson.tasks.junit.TestResultAction.class)
+        
+        if (testDatas) {
+            result = testDatas.result
+            JTtotal = result.getTotalCount().toString()
+            JTfailed = result.getFailCount().toString()
+            JTpassed = result.getPassCount().toString()
+            JTskiped = result.getSkipCount().toString()
+            passRate = result.getPassCount()/result.getTotalCount()*100
+            passRate = passRate.toString()+"%"
+        } else {
+            JTtotal = '0'
+            JTfailed = '0'
+            JTpassed = '0'
+            JTskiped = '0'
+            passRate = '0'
+        }
+        print("Total unit test case Number: " + JTtotal)
+        print("Pass unit test case Number: "+ JTpassed)
+        rtMaven.deployer.addProperty("unittest.summary.total_number",JTtotal)
+        rtMaven.deployer.addProperty("unittest.summary.pass_number",JTpassed)
+        rtMaven.deployer.addProperty("unittest.summary.pass_rate",passRate)
+        
+        
+    }
       stage('xray') {
 
       }
